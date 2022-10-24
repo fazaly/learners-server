d70c70379f5e7e923ff8e3a7c1a9dcb457016ba1
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-10-24 23:05:15 +0600

    environment setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());clear
+
+app.get('/', (req, res) => {
+    res.send('Learner API Running');
+});
+
+app.listen(port, () => {
+    console.log('Learner Server running on port', port);
+});
