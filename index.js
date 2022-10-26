073f441da9d68740b2330e35bcc2719eaa69fc15
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-10-26 23:44:37 +0600

    course & category updated

--- index.js
+++ index.js
@@ -5,10 +5,42 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+// console.log(categories)
+
 app.get('/', (req, res) => {
-    res.send('Learners API Running');
+    res.send('Tech-Learn API running');
 });
 
+
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    
+    {
+        const category_course = courses.filter(n => n.category_id === id);
+    res.send(category_course);
+    console.log('from category', category_course);
+    }
+    
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    console.log(req.params.id)
+    const id = req.params.id;
+    const selectedCourses = courses.find(n => n._id === id);
+    res.send(selectedCourses)
+})
+
 app.listen(port, () => {
-    console.log('Learner Server running on port', port);
-});
+    console.log('Tech-Learn site is running on Port', port)
+})
